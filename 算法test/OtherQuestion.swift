7b6151f3f3a4a0f69add6d116ab152d1a233b4e8
xiaotang <soul_chaoyue@163.com>
2017-06-30 14:24:43 +0800

    分糖果

--- 算法test/OtherQuestion.swift
+++ 算法test/OtherQuestion.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class OtherQuestion: NSObject {
+    ///****** 1 ******
     /// Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
     ///Example:
     ///Input: "Let's take LeetCode contest"
@@ -25,4 +26,23 @@ class OtherQuestion: NSObject {
         return fullStr
     }
 
+    /// ****** 2 ******
+    /// Given an integer array with even length, where different numbers in this array represent different kinds of candies. Each number means one candy of the corresponding kind. You need to distribute these candies equally in number to brother and sister. Return the maximum number of kinds of candies the sister could gain.
+    ///
+    /// - Example 1:
+    ///Input: candies = [1,1,2,2,3,3]
+    ///Output: 3
+    ///Explanation:
+    ///There are three different kinds of candies (1, 2 and 3), and two candies for each kind.
+    ///Optimal distribution: The sister has candies [1,2,3] and the brother has candies [1,2,3], too.
+    ///The sister has three different kinds of candies.
+    /// - Example 2:
+    ///Input: candies = [1,1,2,3]
+    ///Output: 2
+    ///Explanation: For example, the sister has candies [2,3] and the brother has candies [1,1].
+    ///The sister has two different kinds of candies, the brother has only one kind of candies.
+    
+    func distributeCandies(_ candies: [Int]) -> Int {
+        return min(candies.count / 2, Array(Set(candies)).count)
+    }
 }
