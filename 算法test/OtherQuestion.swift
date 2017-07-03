0abf90b6baf2c8a93f32d97af644ca59e736c50b
xiaotang <soul_chaoyue@163.com>
2017-07-03 15:41:25 +0800

    加个编号

--- 算法test/OtherQuestion.swift
+++ 算法test/OtherQuestion.swift
@@ -46,6 +46,7 @@ class OtherQuestion: NSObject {
         return min(candies.count / 2, Array(Set(candies)).count)
     }
     
+    ///****** 3 ******
     /// Write a program that outputs the string representation of numbers from 1 to n.
     
     ///But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
