2beaf48a618916c3aac8ea38b8e99b0ba2d1397b
xiaotang <soul_chaoyue@163.com>
2017-07-13 14:58:17 +0800

    Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.

--- 算法test/OtherQuestion.swift
+++ 算法test/OtherQuestion.swift
@@ -92,4 +92,30 @@ class OtherQuestion: NSObject {
         }
         return arr
     }
+    
+    /// ****** 4 ******
+    ///Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+    
+    ///For example:
+    
+    ///Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
+    
+    ///Note:
+    ///The order of the result is not important. So in the above example, [5, 3] is also correct.
+    ///Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+    func singleNumber(_ nums: [Int]) -> [Int] {
+        var numSet : Set = Set<Int>()
+        
+        for num in nums {
+            if numSet.contains(num) {
+                numSet.remove(num)
+            }else{
+                numSet.insert(num)
+            }
+        }
+        
+        let numArr = Array(numSet)
+        
+        return numArr
+    }
 }
