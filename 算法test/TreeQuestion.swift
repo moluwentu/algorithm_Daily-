da7b6644883339ad5eaeffdfd9656c78c0254de0
xiaotang <soul_chaoyue@163.com>
2017-07-20 16:45:24 +0800

    You need to construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way.
    The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty parenthesis pairs that don't affect the one-to-one mapping relationship between the string and the original binary tree.

--- 算法test/TreeQuestion.swift
+++ 算法test/TreeQuestion.swift
@@ -127,5 +127,44 @@ class TreeQuestion: NSObject {
         getLargestValues(root: root!.left, row: row + 1)
         getLargestValues(root: root!.right, row: row + 1)
     }
+    
+    /// ****** 4 ******
+    ///You need to construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way.
+    
+    ///The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty parenthesis pairs that don't affect the one-to-one mapping relationship between the string and the original binary tree.
+    ///Input: Binary tree: [1,2,3,4]
+    ///      1
+    ///    /   \
+    ///   2     3
+    ///  /
+    /// 4
+    
+    ///Output: "1(2(4))(3)"
+    
+    ///Explanation: Originallay it needs to be "1(2(4)())(3()())",
+    ///but you need to omit all the unnecessary empty parenthesis pairs.
+    ///And it will be "1(2(4))(3)".
+    func tree2str(_ t: TreeNode?) -> String {
+        if t == nil {
+            return ""
+        }
+    
+        let result = String(t!.val) + ""
+        
+        let left = tree2str(t!.left)
+        let right = tree2str(t!.right) + ""
+        
+        if left.isEmpty && right.isEmpty{
+            return result
+        }
+        if left.isEmpty{
+            return result + "()" + "(" + right + ")"
+        }
+        if right.isEmpty {
+            return result + "(" + left + ")"
+        }
+        
+        return result + "(" + left + ")" + "(" + right + ")"
+    }
 
 }
