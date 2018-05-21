ab6d0a6b31570b8255eb1728f1db297e712175de
xiaotang <soul_chaoyue@163.com>
2018-05-21 14:38:01 +0800

    Given an array of integers, return indices of the two numbers such that they add up to a specific target.     You may assume that each input would have exactly one solution, and you may not use the same element twice.

--- 算法test/OtherQuestion.swift
+++ 算法test/OtherQuestion.swift
@@ -195,5 +195,31 @@ class OtherQuestion: NSObject {
         
         return resultStr
     }
+    
+    ///****** 6 ******
+    //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+    //
+    //You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    /// Example
+    //    Given nums = [2, 7, 11, 15], target = 9,
+    //
+    //    Because nums[0] + nums[1] = 2 + 7 = 9,
+    //    return [0, 1].
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var maps = Dictionary<Int, Int>()
+        
+        for (i,num) in nums.enumerated(){
+             let complement = target - nums[i]
+            for (key,value) in maps{
+                if value == complement{
+                    return [key,i]
+                }
+            }
+            maps[i] = num
+        }
+        
+        print("can't find num")
+        return []
+    }
 }
 
