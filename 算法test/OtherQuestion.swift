870bc827d25fa9c9c941a48c3bf01690796433b5
xiaotang <soul_chaoyue@163.com>
2018-06-22 16:00:57 +0800

    A self-dividing number is a number that is divisible by every digit it contains.
    For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
    
    Also, a self-dividing number is not allowed to contain the digit zero.
    
    Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.

--- 算法test/OtherQuestion.swift
+++ 算法test/OtherQuestion.swift
@@ -221,5 +221,44 @@ class OtherQuestion: NSObject {
         print("can't find num")
         return []
     }
+    
+    ///A self-dividing number is a number that is divisible by every digit it contains.
+    
+    ///For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+    
+   ///Also, a self-dividing number is not allowed to contain the digit zero.
+    
+   ///Given a lower and upper number bound, output a list of every possible self dividing number,including the bounds if possible.
+    
+    ///Input:
+    ///left = 1, right = 22
+    ///Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+    
+    ///Note:
+    
+    ///The boundaries of each input argument are 1 <= left <= right <= 10000.
+    
+    func selfDividingNumbers(_ left: Int, _ right: Int) -> [Int] {
+        var arrM = Array<Int>()
+        
+        for i in left...right{
+            if selfDividing(i){
+                arrM.append(i)
+            }
+        }
+        return arrM
+    }
+    
+    func selfDividing(_ num : Int) -> Bool {
+        var x = num
+        while (x > 0) {
+            let d = x % 10
+            x = x / 10
+            if (d == 0 || (num % d != 0)){
+                return false
+            }
+        }
+        return true
+    }
 }
 
