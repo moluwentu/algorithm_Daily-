b62ff33e58f7483cea6d016502511d5c08f0d09f
xiaotang <soul_chaoyue@163.com>
2018-11-19 17:48:10 +0800

    数组区分奇偶并排先后

--- 算法test/OtherQuestion.swift
+++ 算法test/OtherQuestion.swift
@@ -334,5 +334,17 @@ class OtherQuestion: NSObject {
         
         return array
     }
+    ///****** 11 ******
+    ///Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+    
+    ///You may return any answer array that satisfies this condition.
+    
+    func sortArrayByParity(_ A: [Int]) -> [Int] {
+        var arrM = A
+        arrM.sort { (a, b) -> Bool in
+            return a % 2 < b % 2
+        }
+        return arrM
+    }
 }
 
