c8cb67db482b152e4ec048994bf4071375944a51
xiaotang <soul_chaoyue@163.com>
2018-11-27 15:17:16 +0800

    no message

--- 算法test/OtherQuestion.swift
+++ 算法test/OtherQuestion.swift
@@ -202,7 +202,7 @@ class OtherQuestion: NSObject {
     //You may assume that each input would have exactly one solution, and you may not use the same element twice.
     /// Example
     //    Given nums = [2, 7, 11, 15], target = 9,
-    //
+    //-
     //    Because nums[0] + nums[1] = 2 + 7 = 9,
     //    return [0, 1].
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
@@ -345,6 +345,27 @@ class OtherQuestion: NSObject {
             return a % 2 < b % 2
         }
         return arrM
+    }
+    
+    ///****** 12 ******
+    ///    Given a string S of '(' and ')' parentheses, we add the minimum number of parentheses ( '(' or ')', and in any positions ) so that the resulting parentheses string is valid.
+    ///
+    ///    Formally, a parentheses string is valid if and only if:
+    ///    It is the empty string, or
+    ///    It can be written as AB (A concatenated with B), where A and B are valid strings, or
+    ///    It can be written as (A), where A is a valid string.
+    ///    Given a parentheses string, return the minimum number of parentheses we must add to make the resulting string valid.
+    
+    func minAddToMakeValid(_ S: String) -> Int {
+        var ans = 0
+        for str in S {
+            if String(str) == "(" {
+                ans += 1
+            }else{
+                ans -= 1
+            }
+        }
+        return abs(ans)
     }
 }
 
